93ee1bb9f698c13f81f2b76b0960662a96aaddd8
Chris Arceneaux <carcenea@gmail.com>
2022-11-21 13:33:44 -0500

    flipping back
    Signed-off-by: Chris Arceneaux <carcenea@gmail.com>

--- Src/Public/New-AsBuiltReport.ps1
+++ Src/Public/New-AsBuiltReport.ps1
@@ -395,7 +395,8 @@ function New-AsBuiltReport {
         }
         Try {
             if ($Format -contains 'JSON') {
-                $AsBuiltReport | ConvertTo-Json -Depth 10
+                #$AsBuiltReport | ConvertTo-Json -Depth 10
+                $AsBuiltReport
                 $Format.Remove('JSON')
             }
             if (($Format -contains 'Word') -or ($Format -contains 'HTML') -or ($Format -contains 'Text')) {
